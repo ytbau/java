9dd0a8c92140a637d3b0166221e48cab265922c4
ytbau <bauyt008@gmail.com>
2019-03-23 04:16:36 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// > javac HelloWorld.java && java HelloWorld
+
+import static java.lang.System.out;
+
+class HelloWorld {
+	public static void main(String[] args) {
+		out.println("Hello World");
+	}
+}
